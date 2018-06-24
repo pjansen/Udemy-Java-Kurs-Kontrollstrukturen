843cd46587b0636b176d62fda6698f3c69b2308c
Patrick Jansen <pjansen@sunset-design.de>
2018-06-24 19:20:26 +0200

    second commit

---
+++ src/SchlüsselwortIf.java
@@ -0,0 +1,19 @@
+
+public class SchlüsselwortIf {
+
+	public static void main(String[] args) {
+		
+		// Kontrollstrukturen steuern den Programablaufplan
+		
+		// if = falls
+		// else = ansonsten
+		
+		int score = 5000;
+		
+		if (score == 5000) {
+			System.out.println("Herzlichen Glückwunsch, neuer Rekord!");
+		}
+
+	}
+
+}
